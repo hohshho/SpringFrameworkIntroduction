1e4c7fd10416fbd41299aef0a385d1823b1c59a4
hohshho <tkdgur8377@gmail.com>
2022-01-20 22:04:24 +0900

    DI(의존성 주입) 까지

---
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,46 @@
+package com.example.demo.service;
+
+import com.example.demo.Repository.MemberRepository;
+import com.example.demo.Repository.MemoryMemberRepository;
+import com.example.demo.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member){
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m-> {
+                throw new IllegalStateException("이미 존재하는 회원");
+            });
+    }
+
+    // 전체 회원 조회
+    public Optional<Member> findMembers(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
